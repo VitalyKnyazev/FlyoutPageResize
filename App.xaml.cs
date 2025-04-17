92d195a96e975bfad2e2512b0c87cb4e128a3e5b
Vitaly Knyazev <vitaly.knyazev@outlook.com>
2025-04-17 10:36:43 +0100

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace MauiApp8
+{
+	public partial class App
+	{
+		public App()
+		{
+			InitializeComponent();
+		}
+
+		protected override Window CreateWindow(IActivationState? activationState)
+		{
+			var navigationPage = new NavigationPage(new MainPage());
+
+			var flyoutPage = new FlyoutPage
+			{
+				Flyout = new FlyoutContentPage { Title = "Foo" },
+				FlyoutLayoutBehavior = FlyoutLayoutBehavior.SplitOnLandscape,
+				Detail = navigationPage
+			};
+
+			return new Window(flyoutPage);
+		}
+	}
+}
---
+++ FlyoutContentPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp8;
+
+public partial class FlyoutContentPage : ContentPage
+{
+	public FlyoutContentPage()
+	{
+		InitializeComponent();
+	}
+}
